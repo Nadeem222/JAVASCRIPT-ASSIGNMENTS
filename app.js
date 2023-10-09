e8e8b77b9791450135bb618f06d2487d275a8c4f
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2023-10-09 17:14:14 +0500

    some work

---
+++ app.js
@@ -0,0 +1,84 @@
+
+// alert("Hi! Welcome to Page. ")
+
+// alert("Welcome to JS land... \n Happy Coding!")
+
+// alert('Welcome to JS Land...')
+
+// alert('Happy Coding! \n Prevent this page from creating additional dialogs')
+
+
+
+
+function showAlert() {
+    
+    var Question2 = document.getElementById("Question-2");
+    var Question3 = document.getElementById("Question-3");
+    
+    
+    if(Question2.checked){
+        alert("Welcome to JS land... \n Happy Coding!")
+    }else if(Question3.checked){
+        alert('Welcome to JS land...')
+        alert('Happy Coding! \n Prevent this page from creating additional dialogs')
+    }
+    else {
+        alert('Error! Please check the question box and see their answer');
+    }
+};
+
+// CHaper No 2 String Variables
+
+// for print in alert box
+// var studentName = "Syed Nadeem Ali";
+// var fatherName = "Abdul Hakeem";
+// var age = "30 Year";
+// var phoneNumber = '03128892462';
+// var email = 'syednadeemali221@gmail.com';
+
+
+
+// alert("Name :" + " " + studentName);
+// alert("Fahter Name :" + " " + fatherName);
+// alert('Age :' + " " + age);
+// alert('Mobile :' +  " " + phoneNumber);
+// alert('Email :' + email);
+
+
+// funxtion for print in html 
+window.addEventListener('load', function() {
+    var studentName = document.getElementById('name');
+    var fatherName = document.getElementById('fName');
+    var age = document.getElementById('age');
+    var phoneNumber = document.getElementById('phone');
+    var email = document.getElementById('email');
+
+    document.querySelector("#name").innerHTML = "Syed Nadeem Ali"
+    fatherName.textContent = "Abdul Hakeem";
+    age.textContent = "30";
+    phoneNumber.textContent = "0312-8892462";
+    email.textContent = "syednadeemali221@gmail.com";
+
+});
+// function for print in console
+    var studentName = "Syed Nadeem Ali";
+    var fatherName = 'ABdul Hakeem';
+    var age = "30 Year"
+    var email = "syednadeemali221@gmail.com"
+    var phoneNumber = '03128892462';
+
+    
+
+    console.log("Name :" + " " + studentName);
+    console.log("Fahter Name :" + " " + fatherName);
+    console.log('Age :' + " " + age);
+    console.log('Mobile :' +  " " + phoneNumber);
+    console.log('Email :' + email);
+
+
+// question 5
+
+var pizza = "pizza\npizz\npiz\npi\np";
+
+alert(pizza);
+
