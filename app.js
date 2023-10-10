ed8d4fcfd122e1778fe22a2ecdd3410f9d690f29
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2023-10-10 15:41:50 +0500

    chapter 3 complete

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ alert("Hi! Welcome to My Page. ")
 
 
 function showAlert() {
-    
+
     var Question2 = document.getElementById("Question-2");
     var Question3 = document.getElementById("Question-3");
-    
-    
-    if(Question2.checked){
+
+
+    if (Question2.checked) {
         alert("Welcome to JS land... \n Happy Coding!")
-    }else if(Question3.checked){
+    } else if (Question3.checked) {
         alert('Welcome to JS land...')
         alert('Happy Coding! \n Prevent this page from creating additional dialogs')
     }
@@ -46,19 +46,19 @@ function showAlert() {
 
 
 // funxtion for print in html 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     var studentName = "Syed Nadeem Ali";
     var fatherName = 'ABdul Hakeem';
     var age = "30 Year"
     var email = "syednadeemali221@gmail.com"
     var phoneNumber = '03128892462';
-    
-    
-    
+
+
+
     console.log("Name :" + " " + studentName);
     console.log("Fahter Name :" + " " + fatherName);
     console.log('Age :' + " " + age);
-    console.log('Mobile :' +  " " + phoneNumber);
+    console.log('Mobile :' + " " + phoneNumber);
     console.log('Email :' + email);
 
     document.querySelector("#name").innerHTML = studentName;
@@ -91,7 +91,7 @@ alert("My email address is" + " " + question6);
 
 var question7 = "A Smarter Way to learn javaScript"
 
-alert("I am Trying to Learn" + " "+ question7);
+alert("I am Trying to Learn" + " " + question7);
 
 var question9 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
 alert(question9);
@@ -103,5 +103,49 @@ document.querySelector("#q-8").innerHTML = "Yeah! I can write HTML content throu
 document.querySelector("#q-9").innerHTML = question9;
 
 
+// CHapter 3 
+// Question 1
+
+var age = 30;
+
+alert("I am " + " " + age + " " + " year old")
+
+document.querySelector("#chp-3-q-1").innerHTML = `I am ${age} year old`;
 
 
+// Question 2
+
+// Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+function getVisitorCount() {
+    if (localStorage.getItem('visitorCount')) {
+        let count = parseInt(localStorage.getItem('visitorCount')) + 1;
+        localStorage.setItem('visitorCount', count);
+        return count;
+
+    } else {
+        localStorage.setItem('visitorCount', 1);
+        return 1;
+    }
+
+}
+
+const visitCount = getVisitorCount();
+document.querySelector("#visit-count").innerHTML = `You have visited this site ${visitCount} time(s).`;
+
+var birthYear = 1991;
+alert("My birth Year is " + " " + birthYear+"\n Data type of my declare vairable is Number" )
+
+document.querySelector("#chp-3-q-3").innerHTML = `My birth Year is ${birthYear} <br> Data Type of my declare element is number`
+
+var visitorName = prompt("Please enter your name:");
+var productTitle = prompt('Would you like to order');
+var quantity = prompt("How many " + productTitle + "(s) would you like to order?");
+
+var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on NadeemElectronic store.";
+console.log(message);
+
+document.querySelector('#chp-3-q-4').innerHTML = message;
